9272a3f4894ede24c1691ed61e1f6e97eb7c843c
tonerich531 <tonerich531@yahoo.com>
2025-08-13 07:49:34 -0700

    fourth commit

--- books.js
+++ books.js
@@ -1,5 +1,69 @@
+function renderBooks(filter) {
+  const booksWrapper = document.querySelector('.books')
+
+  
+  const books = getBooks();
+
+  if (filter === 'LOW_TO_HIGH') {
+   books.sort((a, b) => a.originalPrice - b.originalPrice);
+  }
+  else if (filter === 'HIGH_TO_LOW') {
+    books.sort((a, b) => b.originalPrice - a.originalPrice);
+  }
+  else if (filter === 'RATINGS') {
+    books.sort((a, b) => b.rating - a.rating);
+  }
+
+  let ratingHTML = "";
+  
+}
 
 
+const booksHtml = books.map(book => {
+  return `<div class="book">
+            <figure class="book__img--wrapper">
+              <img class="book__img" src="${book.url}" alt="">
+            </figure>
+            <div class="book__title">
+              ${book.title}
+            </div>
+            <div class="book__ratings">
+              ${ratingHTML(book.rating)}
+            </div>
+            <div class="book__price">
+              <span>$${book.originalPrice.toFixed(2)}</span> 
+            </div>
+          </div>`
+ })
+ .join('')
+
+ booksWrapper.innerHTML = booksHtml;         
+
+function ratingHTML(rating) {
+  let ratingHTML = ""; 
+  for (let i = 0; i < Math.floor(rating); ++i) {
+    ratingHTML += '<i class="fas fa-star"></i>'
+  }
+
+     if (!Number.isInteger(rating)) {
+      ratingHTML += '<i class="fas fa-star-half-alt"></i>\n'
+      console.log(ratingHTML)
+}
+    return ratingHTML;
+}
+
+
+
+function filterBooks(event) {
+  renderBooks(event.target.value)
+}
+
+
+setTimeout(() => {
+  renderBooks();
+})
+
+ 
 // FAKE DATA
 function getBooks() {
   return [
@@ -92,4 +156,4 @@ function getBooks() {
       rating: 4.5,
     },
   ];
-}
+ }
